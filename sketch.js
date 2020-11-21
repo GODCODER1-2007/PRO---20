0198fdab074f6399ae6487f97b55f2c38d0873c7
GODCODER1-2007 <73296346+GODCODER1-2007@users.noreply.github.com>
2020-11-21 17:03:52 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,42 +1,101 @@
-var car,wall;
 
-var speed,weight;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint = Matter.Constraint;
+var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject
+var rope1,rope2,rope3, rope4,rope5;
+var world;
+
 
 function setup() {
-  createCanvas(1600,400);
-  
-  speed = random(55,90)
-  weight = random(400,1500)
+	createCanvas(1600, 700);
+	rectMode(CENTER);
 
-  car = createSprite(50,200,50,50)
-  car.velocityX = speed;
-  car.shapeColor = color(80,80,80);
 
-  wall = createSprite(1500,200,60, height/2);
-  wall.shapeColor = color(80,80,80)
+	engine = Engine.create();
+	world = engine.world;
+
+	roofObject=new roof(width/2,height/4,width/7,20);
+
+	bobDiameter=40;
+
+	startBobPositionX=width/2;
+	startBobPositionY=height/4+500;
+	bobObject1=new bob(startBobPositionX-bobDiameter*2,startBobPositionY,bobDiameter);
+	bobObject2=new bob(startBobPositionX-bobDiameter,startBobPositionY,bobDiameter);
+	bobObject3=new bob(startBobPositionX,startBobPositionY,bobDiameter);
+	bobObject4=new bob(startBobPositionX+bobDiameter,startBobPositionY,bobDiameter);
+	bobObject5=new bob(startBobPositionX+bobDiameter*2,startBobPositionY,bobDiameter);
+	
+	
+	//Create a Ground
+	
+
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
+
+
+	rope1=new rope(bobObject1.body,roofObject.body,-bobDiameter*2, 0)
+
+	rope2=new rope(bobObject2.body,roofObject.body,-bobDiameter*1, 0)
+	rope3=new rope(bobObject3.body,roofObject.body,0, 0)
+	rope4=new rope(bobObject4.body,roofObject.body,bobDiameter*1, 0)
+	rope5=new rope(bobObject5.body,roofObject.body,bobDiameter*2, 0)
+
+	Engine.run(engine); 
 }
+
 
 function draw() {
-  background(0);  
-  drawSprites();
+  rectMode(CENTER);
+  background(230);
+  roofObject.display();
 
-  if(wall.x-car.x < (car.width+wall.width)/2) 
-  {
-    car.velocityX=0;
-    var deformation = 0.5 * weight * speed *speed/22509;
-    if(deformation>180)
-    {
-      car.shapeColor = color(255,0,0);
-    }
-
-    if(deformation<180 && deformation>100);
-    {
-      car.shapeColor = color(230,230,0);
-    }
-  
-    if(deformation<100)
-    {
-      car.shapeColor = color(0,255,0)
-    }
-  }
+  rope1.display()
+  rope2.display()
+  rope3.display()
+  rope4.display()
+  rope5.display()	
+  bobObject1.display();
+  bobObject2.display();
+  bobObject3.display();
+  bobObject4.display();
+  bobObject5.display(); 
 }
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45});
+
+  	}
+}
+
+
+function drawLine(constraint)
+{
+	bobBodyPosition=constraint.bodyA.position
+	roofBodyPosition=constraint.bodyB.position
+
+	roofBodyOffset=constraint.pointB;
+	
+	roofBodyX=roofBodyPosition.x+roofBodyOffset.x
+	roofBodyY=roofBodyPosition.y+roofBodyOffset.y
+	line(bobBodyPosition.x, bobBodyPosition.y, roofBodyX,roofBodyY);
+}
+
+
+
+
+
+
